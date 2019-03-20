ceaeb4d3bcc29a82f829bc24886513ccdbf8c3bb
Jennifer Wenger <jennifer@techiewebdesigns.com>
2019-03-20 10:45:48 -0400

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args);
+        {
+
+}
+              static void ShowNamesParts()
+
+        {
+            string[] names = { "Alice Everton", "Robert T. Sharp", "Eva Candace Truesdale Olivier", "Mojo", "Wally R. Droid" };
+            foreach (string name in names)
+            {
+                Console.Write("Name: {0} --", name);
+                Console.Write("{0} parts --", name.Split(' ').Length);
+                char separator = ' ';
+                foreach( string part in name.Split(' '))
+                {
+                    Console.Write("{0}{1}", separator, part);
+                    separator = ',';
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
